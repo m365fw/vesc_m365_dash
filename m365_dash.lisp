7afee471631f87ec460bdf7bcecafee5443eae46
kawaiinekololis <marco@ccbluex.net>
2023-04-02 15:58:27 +0200

    replaced progn function with curly brackets
    VESC 6.05 introduced new LispBM features including the curly brackets

--- m365_dash.lisp
+++ m365_dash.lisp
@@ -1,4 +1,4 @@
-; M365 dashboard compability lisp script v0.5 by Netzpfuscher and 1zuna
+; M365 dashboard compability lisp script v0.6 by Netzpfuscher and 1zuna
 ; UART Wiring: red=5V black=GND yellow=COM-TX (UART-HDX) green=COM-RX (button)+3.3V with 1K Resistor
 ; Guide (German): https://rollerplausch.com/threads/vesc-controller-einbau-1s-pro2-g30.6032/
 ; Tested on VESC 6.0 on PRO2 w/ Makerbase 75100 Alu PCB
@@ -81,7 +81,7 @@
 (define feedback 0)
 
 (defun adc-input(buffer) ; Frame 0x65
-    (progn
+    {
         (setvar 'current-speed (* (get-speed) 3.6))
         
         ; Throttle
@@ -110,7 +110,7 @@
         )
 
         (if (= (+ off lock) 0)
-            (progn ; Driving mode
+            { ; Driving mode
                 (if (> current-speed min-speed)
                     (set-current-rel throttle)
                     (set-current-rel 0)
@@ -118,8 +118,8 @@
                 (if (not (= brake 0))
                     (set-brake-rel brake)
                 )
-            )
-            (progn
+            }
+            {
                 (set-current-rel 0) ; No throttle input when off or locked
                 
                 (if (= lock 1) ; Check if it is locked
@@ -129,13 +129,13 @@
                     )
                     (set-brake-rel 0) ; No brake input when off
                 )
-            )
+            }
         )
-    )
+    }
 )
 
 (defun update-dash(buffer) ; Frame 0x64
-    (progn
+    {
         ; mode field (1=drive, 2=eco, 4=sport, 8=charge, 16=off, 32=lock)
         (if (= off 1)
             (bufset-u8 tx-frame 6 16)
@@ -164,10 +164,10 @@
                 (bufset-u8 tx-frame 9 1) ; beep lock
                 (bufset-u8 tx-frame 9 0))
             (if (> feedback 0)
-                (progn
+                {
                     (bufset-u8 tx-frame 9 1)
                     (setvar 'feedback (- feedback 1))
-                )
+                }
                 (bufset-u8 tx-frame 9 0)
             )
         )
@@ -196,35 +196,35 @@
 
         ; write
         (uart-write tx-frame)
-    )
+    }
 )
 
 (defun read-frames()
     (loopwhile t
-        (progn
+        {
             (uart-read-bytes uart-buf 3 0)
             (if (= (bufget-u16 uart-buf 0) 0x55aa)
-                (progn
+                {
                     (setvar 'len (bufget-u8 uart-buf 2))
                     (setvar 'crc len)
                     (if (and (> len 0) (< len 60)) ; max 64 bytes
-                        (progn
+                        {
                             (uart-read-bytes uart-buf (+ len 4) 0)
                             (looprange i 0 len
                                 (setvar 'crc (+ crc (bufget-u8 uart-buf i))))
                             (if (=(+(shl(bufget-u8 uart-buf (+ len 2))8) (bufget-u8 uart-buf (+ len 1))) (bitwise-xor crc 0xFFFF))
                                 (handle-frame (bufget-u8 uart-buf 1))
                             )
-                        )
+                        }
                     )
-                )
+                }
             )
-        )
+        }
     )
 )
 
 (defun handle-frame(code)
-    (progn
+    {
         (if(= code 0x65)
             (adc-input uart-buf)
         )
@@ -232,75 +232,71 @@
         ;(if(= code 0x64)
             (update-dash uart-buf)
         ;)
-    )
+    }
 )
 
 (defun handle-button()
     (if (= presses 1) ; single press
         (if (= off 1) ; is it off? turn on scooter again
-            (progn
+            {
                 (setvar 'off 0) ; turn on
                 (setvar 'feedback 1) ; beep feedback
                 (setvar 'unlock 0) ; Disable unlock on turn off
                 (apply-mode) ; Apply mode on start-up
                 (stats-reset) ; reset stats when turning on
-            )
+            }
             (setvar 'light (bitwise-xor light 1)) ; toggle light
         )
         (if (>= presses 2) ; double press
-            (progn
+            {
                 (if (> (/(- brake-in cal-brk-lo) cal-brk-hi) brk-deadzone) ; if brake is pressed
                     (if (and (= secret-enabled 1) (> (/(- throttle-in cal-thr-lo) cal-thr-hi) thr-deadzone))
-                        (progn
+                        {
                             (setvar 'unlock (bitwise-xor unlock 1))
                             (setvar 'feedback 2) ; beep 2x
                             (apply-mode)
-                        )
-                        (progn
+                        }
+                        {
                             (setvar 'lock (bitwise-xor lock 1)) ; lock on or off
                             (setvar 'feedback 1) ; beep feedback
-                        )
+                        }
                     )
-                    (progn
+                    {
                         (if (= speedmode 1)
-                            (progn
-                                (setvar 'speedmode 4)
-                            )
+                            (setvar 'speedmode 4)
                             (if (= speedmode 2)
-                                (progn
-                                    (setvar 'speedmode 1)
-                                )
-                                (if (or (= speedmode 4))
+                                (setvar 'speedmode 1)
+                                (if (= speedmode 4)
                                     (setvar 'speedmode 2)
                                 )
                             )
                         )
                         (apply-mode)
-                    )
+                    }
                 )
-            )
+            }
         )
     )
 )
 
 (defun handle-holding-button()
-    (progn
+    {
         (if (= (+ lock off) 0) ; it is locked and off?
-            (progn
+            {
                 (setvar 'unlock 0) ; Disable unlock on turn off
                 (apply-mode)
                 (setvar 'off 1) ; turn off
                 (setvar 'feedback 1) ; beep feedback
-            )
+            }
         )
-    )
+    }
 )
 
 (defun reset-button()
-    (progn
+    {
         (setvar 'presstime (systime)) ; reset press time again
         (setvar 'presses 0)
-    )
+    }
 )
 
 ; Speed mode implementation
@@ -329,11 +325,11 @@
 )
 
 (defun configure-speed(speed watts current)
-    (progn
+    {
         (conf-set 'max-speed speed)
         (conf-set 'l-watt-max watts)
         (conf-set 'l-current-max-scale current)
-    )
+    }
 )
 
 ; Apply mode on start-up
@@ -343,37 +339,37 @@
 (spawn 150 read-frames) 
 
 (loopwhile t
-    (progn
+    {
         (if (> buttonold (gpio-read 'pin-rx))
-            (progn
+            {
                 (setvar 'presses (+ presses 1))
                 (setvar 'presstime (systime))
-            )
+            }
             (if (> (- (systime) presstime) 2500) ; after 2500 ms
                 (if (= (gpio-read 'pin-rx) 0) ; check button is still pressed
                     (if (> (- (systime) presstime) 6000) ; long press after 6000 ms
-                        (progn
+                        {
                             (if (<= (get-speed) button-safety-speed)
                                 (handle-holding-button)
                             )
                             (reset-button) ; reset button
-                        )
+                        }
                     )
-                    (progn ; when button not pressed
+                    { ; when button not pressed
                         (if (> presses 0) ; if presses > 0
-                            (progn
+                            {
                                 (if (<= (get-speed) button-safety-speed)
                                     (handle-button) ; handle button presses
                                 )
                                 (reset-button) ; reset button
-                            )
+                            }
                         )
-                    )
+                    }
                 )
             )
         )
 
         (setvar 'buttonold (gpio-read 'pin-rx))
         (sleep 0.05) ; Recude load on the CPU
-    )
+    }
 )
