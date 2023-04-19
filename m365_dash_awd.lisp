c9810ea624f20a9e1e2c8a62f8218414b40f8a87
kawaiinekololis <marco@ccbluex.net>
2023-04-19 18:38:04 +0200

    Added +2 km/h to secret modes due to soft limiter

--- m365_dash_awd.lisp
+++ m365_dash_awd.lisp
@@ -33,10 +33,10 @@
 (define sport-watts 700) ; or 1500000
 
 (define secret-enabled 1)
-(define secret-eco-speed (/ 25 3.6))
+(define secret-eco-speed (/ 27 3.6))
 (define secret-eco-current 0.6)
 (define secret-eco-watts 1500000)
-(define secret-drive-speed (/ 45 3.6))
+(define secret-drive-speed (/ 47 3.6))
 (define secret-drive-current 0.8)
 (define secret-drive-watts 1500000)
 (define secret-sport-speed (/ 400 3.6))
